0a93e834be3890742081f93127114b837a903ecc
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-04-24 17:25:41 +0300

    Update problem1.py

--- Python/problem1.py
+++ Python/problem1.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def isPalindrome(self, x):
+        reverse=0
+        num=x
+        while x>0:
+            remainder = x % 10
+            reverse = (reverse * 10) + remainder
+            x= x/10
+        if reverse==num:
+            return True
+        else:
+            return False 
