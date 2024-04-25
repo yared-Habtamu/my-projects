17865b039079ff7c6c6bc442f14fbf1375b9ac03
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-04-26 02:28:19 +0300

    Update p5.py

--- Python/p5.py
+++ Python/p5.py
@@ -1,1 +1,9 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        answer=[]
+        kelvin=float(celsius+273.15)
+        fahrenheit=float((celsius*1.80)+32.00)
+        answer.append(kelvin)
+        answer.append(fahrenheit)
 
+        return answer
