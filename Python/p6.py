baedd9d5b9111c3b30e405ebb3d65fd20e096eb3
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-04-26 02:32:03 +0300

    Update p6.py

--- Python/p6.py
+++ Python/p6.py
@@ -1,1 +1,12 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        n=len(nums)
+        ans=[]
+        for i in range(2*n):
+            if i<=n-1:
+                ans.append(nums[i])
+            else:
+                ans.append(nums[i-n])
+        return ans
+
 
