dc7283a5c909e460ae7752fe57efccb76dacdf38
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-04-26 01:28:11 +0300

    Update p4.py

--- Python/p4.py
+++ Python/p4.py
@@ -1,1 +1,24 @@
+class Solution(object):
+    def maxArea(self, height):
+        maxa = 0
+        l = 0
+        r = len(height) - 1
 
+        while l < r:
+            if height[l]<height[r]:
+                h=height[l]
+            else:
+                h=height[r]
+            w = r - l
+            area = h * w
+            if maxa<area:
+                maxa=area
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return maxa
+
+#Leet Code 11, Container with most water 
