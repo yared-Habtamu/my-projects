f95c988a35e3b7d2ffc6f5363b9af59af7faae56
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-04-26 03:24:12 +0300

    Update p10.py

--- Python/p10.py
+++ Python/p10.py
@@ -1,1 +1,17 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        prod = 1
+        answer = []
+        m = len(nums)
+        for j in range(m):
+            for i in range(m):
+                if j != i:
+                    prod = prod * nums[i]
+                    if i == m - 1:
+                        answer.append(prod)
+                        prod = 1
+                elif i == j == m - 1:
+                    answer.append(prod)
+        return answer
+
 
