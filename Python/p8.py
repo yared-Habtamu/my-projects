37d0b734cfdc6843c189446808976cc5043b1bb8
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-04-26 03:04:42 +0300

    Update p8.py

--- Python/p8.py
+++ Python/p8.py
@@ -1,1 +1,10 @@
+class Solution(object):
+    def sortColors(self, nums):
+        for i in range(len(nums)):
+            for j in range(1,len(nums)):
+                if nums[j]<=nums[j-1]:
+                    temp=nums[j]
+                    nums[j]=nums[j-1]
+                    nums[j-1]=temp
 
+#75. Sort Colors  I just use bubble sort approach
