e2da59b708b7bbbd8b34a8d8f2d802d602922be1
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-04-29 18:03:47 +0300

    Update valid_parenthesis.py

--- Python/valid_parenthesis.py
+++ Python/valid_parenthesis.py
@@ -1,1 +1,30 @@
+class Solution(object):
+    def isValid(self, s):
+        list2=[]
+        for i in range(0,len(s)-1):
+            if s[i]=='(':
+                for j in range(i+1,len(s)):
+                    if s[j]==')':
+                        list2.append(s[i])
+                        list2.append(s[j])
+                    elif j==len(s):
+                        list2.append(s[i])
+            elif s[i]=='[':
+                for j in range(i+1,len(s)):
+                    if s[j]==']':
+                        list2.append(s[i])
+                        list2.append(s[j])
+                    elif j==len(s):
+                        list2.append(s[i])          
+            elif s[i]=='{':
+                for j in range(i+1,len(s)):
+                    if s[j]=='}':
+                        list2.append(s[i])
+                        list2.append(s[j])
+                    elif j==len(s):
+                        list2.append(s[i])
+        if (len(list2)%2)!=0:
+            return False
+        return True
 
+ 
