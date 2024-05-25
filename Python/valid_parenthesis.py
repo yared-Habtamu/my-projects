854b39f8c65fbfe94b062a6cff13f183b7d3cb78
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-05-25 14:40:12 +0300

    Update valid_parenthesis.py

--- Python/valid_parenthesis.py
+++ Python/valid_parenthesis.py
@@ -1,30 +1,15 @@
 class Solution(object):
     def isValid(self, s):
-        list2=[]
-        for i in range(0,len(s)-1):
-            if s[i]=='(':
-                for j in range(i+1,len(s)):
-                    if s[j]==')':
-                        list2.append(s[i])
-                        list2.append(s[j])
-                    elif j==len(s):
-                        list2.append(s[i])
-            elif s[i]=='[':
-                for j in range(i+1,len(s)):
-                    if s[j]==']':
-                        list2.append(s[i])
-                        list2.append(s[j])
-                    elif j==len(s):
-                        list2.append(s[i])          
-            elif s[i]=='{':
-                for j in range(i+1,len(s)):
-                    if s[j]=='}':
-                        list2.append(s[i])
-                        list2.append(s[j])
-                    elif j==len(s):
-                        list2.append(s[i])
-        if (len(list2)%2)!=0:
-            return False
-        return True
+        stack = []
+        bracket = {')': '(', ']': '[', '}': '{'}        
+        for ch in s:
+            if ch in bracket:
+                top_element = stack.pop() if stack else '#'
+                if bracket[ch] != top_element:
+                    return False
+            else:
+                stack.append(ch)
+        
+        return not stack
 
  
