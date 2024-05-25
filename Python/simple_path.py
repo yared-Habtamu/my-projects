441057d716b323689b514317558d487c8ae3054b
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-05-25 15:10:50 +0300

    Create simple_path.py

---
+++ Python/simple_path.py
@@ -0,0 +1,16 @@
+class Solution:
+    def simplifyPath(self, path):
+        dir_stack = []
+        path = path.split("/")
+        for elem in path:
+            if dir_stack and elem == "..":
+                dir_stack.pop()
+            elif elem not in [".", "", ".."]:
+                dir_stack.append(elem)
+                
+        return "/" + "/".join(dir_stack)
+
+    """
+    :type path: str
+    :rtype: str
+    """
