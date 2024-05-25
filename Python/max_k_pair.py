41658e833c6c4f1e45d49ca41ad8a324325f3829
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-05-25 14:33:59 +0300

    Create max_k_pair.py

---
+++ Python/max_k_pair.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxOperations(self, nums, k):
+        nums.sort()
+        left = 0
+        right = len(nums) - 1
+        ans = 0
+        while left < right:
+            cur = nums[left] + nums[right]
+            if cur == k:
+                ans += 1
+                left += 1
+                right -= 1
+            elif cur < k:
+                left += 1
+            else:
+                right -= 1
+        
+        return ans
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
