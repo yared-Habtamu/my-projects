ff89a53673016448d01c207605b9314d48cc11f2
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-06-20 16:52:22 +0300

    Create sort_col.py

---
+++ Python/sort_col.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def sortColors(self, nums):
+        for i in range(len(nums)):
+            for j in range(1,len(nums)):
+                if nums[j]<=nums[j-1]:
+                    temp=nums[j]
+                    nums[j]=nums[j-1]
+                    nums[j-1]=temp
