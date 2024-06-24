4e692aed1398bf9bf5da8a6023d7aaee164287dd
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-06-24 09:03:40 +0300

    Update smaller_than_current.py

--- Python/smaller_than_current.py
+++ Python/smaller_than_current.py
@@ -1,14 +1,10 @@
 class Solution(object):
     def smallerNumbersThanCurrent(self, nums):
         ans=[]    
-        for i in range(len(nums)):
-            c=0
-            for j in range(len(nums)):
-                if i!=j and nums[j]<nums[i]:
-                    c+=1
-            ans.append(c)
-            c=0
-        
+        x=sorted(nums)
+        for num in nums:
+            i=x.index(num)
+            ans.append(i)
         return ans
             
 
