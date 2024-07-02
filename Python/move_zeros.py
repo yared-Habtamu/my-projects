2b2ea7e4a0e09896b935416bea30ca51c00aef5f
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-07-02 13:30:02 +0300

    Update move_zeros.py

--- Python/move_zeros.py
+++ Python/move_zeros.py
@@ -1,6 +1,8 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        for i in range(len(nums)):
-            if nums[i]==0:
-                nums.remove(nums[i])
-                nums.append(0)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        holder,seeker=0,0
+        while seeker<len(nums):
+            if nums[seeker]!=0:
+                nums[holder],nums[seeker]=nums[seeker],nums[holder]
+                holder+=1
+            seeker+=1
