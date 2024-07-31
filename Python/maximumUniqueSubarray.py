6c7ab24f2aee7fba327b101419b57726e17dfa8a
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-07-31 16:30:44 +0300

    Create maximumUniqueSubarray.py

---
+++ Python/maximumUniqueSubarray.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maximumUniqueSubarray(self, nums):
+        slow = 0
+        fast = 0
+        tot = 0
+        max_tot = 0
+        window = set()
+
+        while fast < len(nums):
+            if nums[fast] not in window:
+                window.append(nums[fast])
+                tot += nums[fast]
+                max_tot = max(max_tot, tot)
+                fast += 1
+            else:
+                window.remove(nums[slow])
+                tot -= nums[slow]
+                slow += 1
+
+        return max_tot
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
